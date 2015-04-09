f31d5bbff2b4cec7137a1a65a7b34ff5d95500f0
cole <cole@mbuntu.com>
2015-04-09 10:16:25 +0800

    + linux-shdowsocks-global.sh: @TODO clean fules

--- scripts/linux-shdowsocks-global.sh
+++ scripts/linux-shdowsocks-global.sh
@@ -23,7 +23,7 @@ function random ()
 }
 
 # random SS_PORT
-random;
+# random;
 
 if [ ! -f $SS_CONFIG_FILE ]; then
     echo "First Use, Please Config Shadowsocks:"
@@ -37,6 +37,30 @@ if [ ! -f $SS_CONFIG_FILE ]; then
     read SS_PASSWORD
     echo -n "Server Method (Default: aes-256-cfb): "
     read SS_METHOD
+    # save config to $SS_CONFIG_FILE
+    echo "{" > $SS_CONFIG_FILE
+    echo "  \"server\": \"$SS_IP\"," >> $SS_CONFIG_FILE
+    echo "  \"server_port\": $SS_PORT," >> $SS_CONFIG_FILE
+    echo "  \"local_port\": $SS_LOCAL_PORT," >> $SS_CONFIG_FILE
+    echo "  \"password\": \"$SS_PASSWORD\"," >> $SS_CONFIG_FILE
+    echo "  \"method\": \"$SS_METHOD\"" >> $SS_CONFIG_FILE
+    echo "}" >> $SS_CONFIG_FILE
+    # save end
+else
+# read config
+SS_IP=$(cat $SS_CONFIG_FILE| grep -i server | head -n 1 | awk -F '"' '{print $4}')
+SS_PORT=$(cat $SS_CONFIG_FILE| grep -i server_port | head -n 1 | awk -F ' ' '{print $2}' | awk -F ',' '{print $1}')
+SS_LOCAL_PORT=$(cat $SS_CONFIG_FILE| grep -i local_port | head -n 1 | awk -F ' ' '{print $2}' | awk -F ',' '{print $1}')
+SS_PASSWORD=$(cat $SS_CONFIG_FILE| grep -i password | head -n 1 | awk -F '"' '{print $4}')
+SS_METHOD=$(cat $SS_CONFIG_FILE| grep -i method | head -n 1 | awk -F '"' '{print $4}')
+
+# echo config
+echo "Use Old Config in $SS_CONFIG_FILE:"
+echo "server: $SS_IP"
+echo "server_port: $SS_PORT"
+echo "local_port: $SS_LOCAL_PORT"
+echo "password: $SS_PASSWORD"
+echo "method: $SS_METHOD"
 fi
 
 # root
@@ -46,23 +70,24 @@ fi
 # fi
 
 # iptables
-which iptables;
+which iptables >> /dev/null;
 if [ "$?" != "0" ]; then
     echo "Please install iptables first.";
     exit;
 fi
 
 # shadowsocks
-which $SS
+which $SS >> /dev/null
 if [ "$?" != "0" ]; then
     echo "Please install shadowsocks first."
     exit
 fi 
 
 # iptables
-sudo ptables -t nat -L $CHAIN_NAME >> /dev/null
+sudo iptables -t nat -L $CHAIN_NAME >> /dev/null
 if [ "$?" != "0" ]; then
     sudo iptables -t nat -N $CHAIN_NAME
+    sleep 3
 fi
 sudo iptables -t nat -F
 
@@ -80,32 +105,21 @@ sudo iptables -t nat -A $CHAIN_NAME -d 224.0.0.0/4 -j RETURN
 sudo iptables -t nat -A $CHAIN_NAME -d 240.0.0.0/4 -j RETURN
 
 ## Anything else should be redirected to shadowsocks's local port
-sudo iptables -t nat -A $CHAIN_NAME -j REDiRECT --to-ports $SS_LOCAL_PORT
+sudo iptables -t nat -A $CHAIN_NAME -p tcp -j REDIRECT --to-ports $SS_LOCAL_PORT
+sudo iptables -t nat -A $CHAIN_NAME -p udp -j REDIRECT --to-ports $SS_LOCAL_PORT
 
 # Apply the rules
-sudo iptables -t nat -L OUTPUT | grep $SHADOWSOCKS >> /dev/null
+sudo iptables -t nat -L OUTPUT | grep $CHAIN_NAME >> /dev/null
 if [ "$?" != "0" ]; then 
     sudo iptables -t nat -I OUTPUT -j $CHAIN_NAME
 fi
 
 ## SAVE iptables config
-sudo iptables-save
+sudo iptables-save >> /dev/null
 
 # config shadowsocks
 if [ ! -d "$HOME/.config/shadowsocks" ]; then
-    mkdir $HOME/.config/shadowsocks;
-fi
-if [ ! -f "$SS_CONFIG_FILE" ]; then
-    echo "{" > $SS_CONFIG_FILE
-    echo "  \"server\": \"$SS_IP\"," >> $SS_CONFIG_FILE
-    echo "  \"server_port\": $SS_PORT," >> $SS_CONFIG_FILE
-    echo "  \"local_port\": $SS_LOCAL_PORT," >> $SS_CONFIG_FILE
-    echo "  \"password\": \"$SS_PASSWORD\"," >> $SS_CONFIG_FILE
-    echo "  \"method\": \"$SS_PASSWORD\"" >> $SS_CONFIG_FILE
-    echo "}" >> $SS_CONFIG_FILE
-else
-    echo "ConfigFile Already Exist: $SS_CONFIG_FILE"
-    echo "Use Old Config."
+    mkdir -p $HOME/.config/shadowsocks;
 fi
 
 # Start the shadowsocks-redir
@@ -113,5 +127,10 @@ if [ "$1" == "-d" ]; then
     echo "Shadowsocks Daemon Now."
     $SS -c $SS_CONFIG_FILE -f /tmp/shadowsocks.pid
 else 
+    echo "Start shadowsocks redirect ..."
     $SS -c $SS_CONFIG_FILE
+
+    # exit flush all rules
+    # catch signal: ctrl + c
+    # trap 'echo "Cleaning Fire Rules...";sudo iptables -t nat -F $CHAIN_NAME;sudo iptables -t nat -X $CHAIN_NAME;echo "Cleaning end.";exit -1;' SIGINT
 fi
