ee5a3da01339f9204fcb057fb3d1543f63aba688
whatwewant <tobewhatwewant@gmail.com>
2015-04-17 01:09:37 +0800

    + config_vim.sh: new vimrc

--- scripts/config_vim.sh
+++ scripts/config_vim.sh
@@ -2,18 +2,19 @@
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 
 # Backup Old Config
-if [ -f "~/.vimrc" ] || [ -d "~/.vim" ]; then
-	sudo mv ~/.vimrc ~/.vim >> /dev/null 2>&1
-    sudo mv ~/.vim ~/.vim_backup-$(date +%Y-%m-%d) >> /dev/null 2>&1
-fi
+# if [ -f "~/.vimrc" ] || [ -d "~/.vim" ]; then
+# sudo mv ~/.vimrc ~/.vim >> /dev/null 2>&1
+# sudo mv ~/.vim ~/.vim_backup-$(date +%Y-%m-%d) >> /dev/null 2>&1
+# fi
+sudo rm -rf ~/.vim*
 
 # git clone https://github.com/whatwewant/vim.git ~/.vim
 # mv -f ~/.vim/.vimrc ~/
 wget http://whatwewant.github.io/confs/vimrc -O ~/.vimrc
 
 # bundle
-mkdir ~/.vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+mkdir -p ~/.vim/bundle && \
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
@@ -30,6 +31,7 @@ fi
 echo "正在努力为您安装bundle程序" > potter
 echo "安装完毕将自动退出" >> potter
 echo "请耐心等待" >> potter
-vim potter -c "BundleInstall" -c "q" -c "q"
-rm potter
+# vim potter -c "PulginInstall" -c "q" -c "q"
+# rm potter
+vim +PluginInstall +qall
 echo "安装完成"
