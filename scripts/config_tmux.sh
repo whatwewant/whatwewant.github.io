ce1d2f55820b89f04533141fda302ce4a5da188d
whatwewant <tobewhatwewant@gmail.com>
2015-04-17 08:25:49 +0800

    + config_tmux.sh ... and new tmux.conf

--- scripts/config_tmux.sh
+++ scripts/config_tmux.sh
@@ -6,16 +6,21 @@ PROGRAM_NAME=tmux
 DESTINATION=~/.tmux/plugins/tpm
 URL="https://github.com/ThomasAdam/tmux.git"
 
+bash_url() {
+    url=$1
+    script_name=bscript
+    wget $url -O /tmp/$script_name && \
+        bash /tmp/$script_name
+}
+
 which tmux >> /dev/null
 if [ "$?" != "0" ]; then
-    wget https://whatwewant.github.io/scripts/install_tmux.sh -O /tmp/install_tmux
-    bash /tmp/install_tmux
+    bash_url https://whatwewant.github.io/scripts/install_tmux.sh
 fi
 
 tmux -V | grep -i 2
 if [ "$?" != "0" ]; then
-    wget https://whatwewant.github.io/scripts/install_tmux.sh -O /tmp/install_tmux
-    bash /tmp/install_tmux
+    bash_url https://whatwewant.github.io/scripts/install_tmux.sh
 fi
 
 # DESTINATION
@@ -23,7 +28,12 @@ echo "Cloning $PROGRAM_NAME to $DESTINATION ..."
 git clone $URL $DESTINATION
 
 # Config file .tmux.conf
+echo "Backup $HOME/.tmux.conf to $HOME/.backup/tmux.conf ..."
+mkdir ~/.backup >> /dev/null 2>&1
+mv ~/.tmux.conf ~/.backup/tmux.conf 2&>1
+echo "Get new .tmux.conf ..."
 wget https://whatwewant.github.io/confs/tmux.conf -O ~/.tmux.conf
 
 # Reload TMUX environment so TPM is sourced:
+echo "Reload TMUX environment ..."
 tmux source-file ~/.tmux.conf
