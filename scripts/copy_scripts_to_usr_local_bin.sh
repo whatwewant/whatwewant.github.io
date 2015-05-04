855065adf8477f658ff84f1358588fe59c0cd636
cole <cole@mbuntu.com>
2015-05-04 19:13:45 +0800

    + ConfigUbuntuDesktop.sh: reconstruct code

---
+++ scripts/copy_scripts_to_usr_local_bin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# File Name: config_init_background_image.sh
+# Author: Potter
+# Mail: tobewhatwewant@gmail.com
+# Created Time: 2015年05月04日 星期一
+
+CURRENT_PATH=$(cd `dirname $0`; pwd)
+
+COPY () {
+    LOCAL_FILE=$CURRENT_PATH/$1
+    NEW_NAME=/usr/local/bin/$2
+    [[ ! -d "/usr/local/bin" ]] && sudo mkdir -p /usr/local/bin
+    sudo cp $LOCAL_FILE $NEW_NAME
+}
+
+# 1. control cpu frequence
+COPY control_cpu_frequent.sh control_cpu_frequence
+
+# 2. shadowsocks_global
+COPY linux-shdowsocks-global.sh shadowsocks_global
+
