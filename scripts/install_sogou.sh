9e2a2ef3d75c6f11dc890e6c27d918d6be5355de
cole <cole@mbuntu.com>
2015-05-06 13:52:18 +0800

    + add new install_PACKAGE templete: like install_sogou.sh install_wps.sh

---
+++ scripts/install_sogou.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# set -e
+
+# downloadTool="sudo apt-get install -y"
+PACKAGE_DIR=/tmp/src
+# SRC_DIR=$PACKAGE_DIR
+# SRC_DIR_FINAL=${SRC_DIR}/shadowsocks
+# URL="https://github.com/madeye/shadowsocks-libev.git"
+# BINARY_DIR=/usr/local/sbin
+CONFIG_DIR=$HOME/.config/Cud
+LOG_FILE=$CONFIG_DIR/INSTALL_PACKAGES.log
+
+dpkgLog() {
+    # 使用前修复依赖
+    sudo apt-get -f -y install >> /dev/null 2>&1
+
+    for argv in "$@"
+    do
+        sudo dpkg -i $PACKAGE_DIR/$argv && \
+            echo "(DPKG)Succeed in installing --> $argv $(date)" >> $LOG_FILE || \
+            echo "(DPKG)ERROR: Failed to install --> $argv $(date)" >> $LOG_FILE
+        sudo apt-get -f -y install >> /dev/null 2>&1
+    done
+}
+
+wgetTo () {
+    wTurl=$1
+    wTnewname=$2
+    wget -c $wTurl -O $PACKAGE_DIR/$wTnewname
+}
+
+wgetThenDpkg () {
+    wTDurl=$1
+    wTDname=$2
+    wgetTo $wTDurl $wTDname && \
+        dpkgLog $wTDname
+}
+
+initialize () {
+    [[ ! -d $CONFIG_DIR ]] && mkdir -p $CONFIG_DIR
+    [[ ! -d $PACKAGE_DIR ]] && mkdir -p $PACKAGE_DIR
+}
+
+# Begin
+initialize
+
+# download and install packages
+PACKAGE_URL="http://pinyin.sogou.com/linux/download.php?f=linux&bit=64"
+wgetThenDpkg $PACKAGE_URL sougou.deb
