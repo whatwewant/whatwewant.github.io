2044f6473c55b7227c6d64fb971b0cec67b3b9cb
cole <cole@mbuntu.com>
2015-05-09 20:05:55 +0800

    + BaseFunctionSets.sh: 自定义脚本函数集合

---
+++ scripts/BaseFunctionSets.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+# set -e
+
+CURRENT_PATH=$(cd `dirname $0`; pwd)
+
+# downloadTool="sudo apt-get install -y"
+PACKAGE_DIR=/tmp/src
+# SRC_DIR=$PACKAGE_DIR
+# SRC_DIR_FINAL=${SRC_DIR}/shadowsocks
+# URL="https://github.com/madeye/shadowsocks-libev.git"
+# BINARY_DIR=/usr/local/sbin
+CONFIG_DIR=$HOME/.config/Cud
+LOG_FILE=$CONFIG_DIR/INSTALL_PACKAGES.log
+
+menu() {
+    echo -e "
+    Function Sets 
+        (For detail: $0 about FUNCTION_NAME):
+
+    dpkgLog
+    wgetTo 
+    wgetThenDpkg 
+    initialize 
+    downloadLog
+    pipLog
+    pip3Log
+    easy_installLog
+    makeLog
+    gitThenMake
+    "
+}
+
+initialize () {
+    # Usage:
+    #   initialize
+    sudo ls /tmp >> /dev/null 2>&1
+    [[ ! -d $CONFIG_DIR ]] && mkdir -p $CONFIG_DIR
+    [[ ! -d $PACKAGE_DIR ]] && mkdir -p $PACKAGE_DIR
+    export PATH=$CURRENT_PATH:$PATH
+}
+
+dpkgLog() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  dpkgLog path/to/package1 path/to/package2 ..."
+        exit 0
+    fi
+
+    # 使用前修复依赖
+    sudo apt-get -f -y install >> /dev/null 2>&1
+
+    for argv in "$@"
+    do
+        sudo dpkg -i $PACKAGE_DIR/$argv && \
+            echo "(DPKG)Succeed in installing --> $argv $(date)" >> $LOG_FILE || \
+            echo "(DPKG)ERROR: Failed to install --> $argv $(date)" >> $LOG_FILE
+        sudo apt-get -f -y install >> /dev/null 2>&1
+    done
+}
+
+wgetTo () {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  wgetTo 下载链接URL 存取文件名"
+        exit 0
+    fi
+    wTurl=$1
+    wTnewname=$2
+    wget -c $wTurl -O $PACKAGE_DIR/$wTnewname
+}
+
+wgetThenDpkg () {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  wgetThenDpkg 下载链接URL 存取文件名"
+        exit 0
+    fi
+
+    wTDurl=$1
+    wTDname=$2
+    wgetTo $wTDurl $wTDname && \
+        dpkgLog $wTDname
+}
+
+downloadLog() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  downloadLog path/to/package1 path/to/package2 ..."
+        exit 0
+    fi
+    # 使用前修复依赖
+    sudo apt-get -f -y install
+
+    for argv in "$@"
+    do
+        sudo apt-get install -y $argv && \
+        echo "(APT-GET)Succeed in installing --> $argv $(date)" >> $LOG_FILE || \
+        echo "(APT-GET)ERROR: Failed to install --> $argv $(date)" >> $LOG_FILE
+    done
+}
+
+pipLog() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  pipLog path/to/package1 path/to/package2 ..."
+        exit 0
+    fi
+    for argv in "$@"
+    do
+        sudo pip install $argv && \
+            echo "(PIP)Succeed in installing --> $argv" >> $LOG_FILE || \
+            echo "(PIP)ERROR: Failed to install $argv" >> $LOG_FILE
+    done
+}
+
+pip3Log() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  pip3Log path/to/package1 path/to/package2 ..."
+        exit 0
+    fi
+    for argv in "$@"
+    do
+        sudo pip3 install $argv && \
+            echo "(PIP3)Succeed in installing --> $argv" >> $LOG_FILE || \
+            echo "(PIP3)ERROR: Failed to install $argv" >> $LOG_FILE
+    done
+}
+
+easy_installLog() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  easy_installLog path/to/package1 path/to/package2 ..."
+        exit 0
+    fi
+
+    for argv in "$@"
+    do
+        sudo easy_install $argv && \
+            echo "(EASY_INSTALL)Succeed in installing --> $argv" >> $LOG_FILE || \
+            echo "(EASY_INSTALL)ERROR: Failed to install $argv" >> $LOG_FILE
+    done
+}
+
+makeLog() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  makeLog PROGRAM_NAME"
+        exit 0
+    fi
+    PROGRAM_NAME=$1
+    make_dir=$PACKAGE_DIR/$PROGRAM_NAME
+    cd $make_dir
+    make clean
+    make && sudo make install && \
+        echo "(MAKE)Succeed in installing --> $PROGRAM_NAME $(date)" >> $LOG_FILE || \
+        echo "(MAKE)ERROR: Failed to install $PROGRAM_NAME $(date)" >> $LOG_FILE
+}
+
+gitThenMake() {
+    if [ "$1" == "about" ]; then
+        echo "Usage:"
+        echo "  gitThenMake 项目地址URL 自定义项目名"
+        exit 0
+    fi
+    gTMURL=$1
+    gTMNAME=$2
+    git clone $gTMURL $PACKAGE_DIR/$gTMNAME && \
+        make_log $gTMNAME
+}
+
+# 初始化
+initialize;
+
+case $1 in
+    -h|--help)
+        menu
+        ;;
+    about)
+        [[ "$2" = "" ]] && menu && exit 0
+        $2 about 2>>/dev/null
+        [[ "$?" != "0" ]] && echo "Function $2 doesnot exist."
+        ;;
+    *)
+        menu
+        ;;
+esac
