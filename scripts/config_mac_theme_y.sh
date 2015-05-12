e84fa27a28425f4da764adf8375fa7c7c8dc071b
whatwewant <tobewhatwewant@gmail.com>
2015-05-12 23:21:31 +0800

    + config_mac_theme_y.sh: for ubuntu 14.04/15.04

--- scripts/config_mac_theme_y.sh
+++ scripts/config_mac_theme_y.sh
@@ -19,15 +19,10 @@ case "$UBUNTU_VERSION" in
         ;;
 esac
 
-# Replace 'Ubuntu Desktop' text with 'Mac' on the Panel
-cd /usr/share/locale/en/LC_MESSAGES/
-sudo msgfmt -o unity.mo ${script_path}/../icons/Mac.po
-
-# Apple Logo in Launcher
-sudo cp ${script_path}/../icons/launcher_bfb.png /usr/share/unity/icons/
 
 ## 安装mac主题
 sudo apt-get install -y unity-tweak-tool
+# sudo apt-get install -y gnome-tweak-tool
 ## docky
 sudo apt-get install -y docky
 ## 添加mac主题源 www.noobslab.com
@@ -35,6 +30,12 @@ sudo apt-get install -y docky
 # sudo apt-get update
 case "$UBUNTU_VERSION" in
     "14.04")
+        # Replace 'Ubuntu Desktop' text with 'Mac' on the Panel
+        cd /usr/share/locale/en/LC_MESSAGES/
+        sudo msgfmt -o unity.mo ${script_path}/../icons/Mac.po
+        
+        # Apple Logo in Launcher
+        sudo cp ${script_path}/../icons/launcher_bfb.png /usr/share/unity/icons/
         # 安装mac主题, unity-tweak-tool 选择 Theme(主题) 选择Mbutun-x
         sudo apt-get install -y mbuntu-y-ithemes-v4
         ## 安装mac主题图标, unity-tweak-tool 选择 Icons(图标) 选择Mbuntu-sl
@@ -49,6 +50,17 @@ case "$UBUNTU_VERSION" in
         sudo apt-get install -y mbuntu-y-ithemes-v5
         sudo apt-get install -y mbuntu-y-icons-v5
         sudo apt-get install mbuntu-y-docky-skins-v5
+        cd /tmp && wget -O config.sh http://drive.noobslab.com/data/Mac-15.04/config.sh
+        chmod +x config.sh;./config.sh
+        cd /tmp && wget -O Mac.po http://drive.noobslab.com/data/Mac-15.04/change-name-on-panel/mac.po
+        cd /usr/share/locale/en/LC_MESSAGES; sudo msgfmt -o unity.mo ~/Mac.po;rm ~/Mac.po; cd /tmp
+        wget -O launcher_bfb.png http://drive.noobslab.com/data/Mac-15.04/launcher-logo/apple/launcher_bfb.png
+        sudo mv launcher_bfb.png /usr/share/unity/icons/
+        cd /tmp; wget -O mac-fonts.zip http://drive.noobslab.com/data/Mac-15.04/macfonts.zip
+        sudo unzip mac-fonts.zip -d /usr/share/fonts; rm mac-fonts.zip
+        sudo fc-cache -f -v
+        sudo apt-get install -y libreoffice-style-sifr
+        sudo apt-get install -y mbuntu-y-lightdm-v5
         ;;
 esac
 
@@ -57,4 +69,5 @@ sudo apt-get install -y slingscold
 ## Indicator Synapse and Mutate (Alternative to Spotlight)
 sudo apt-get install -y indicator-synapse
 ## Apply MBuntu Splash
-
+# Mutate Spolight
+sudo apt-get install mutate
