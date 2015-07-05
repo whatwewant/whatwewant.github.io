9f3d0a0f2193011b4a1075f6acbb2ecc7e6c2c6e
cole <cole@mbuntu.com>
2015-07-05 08:38:41 +0800

    + glup start.sh: + NODE_PATH

--- confs/gulp/start.sh
+++ confs/gulp/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "node_modules" ]; then
+if [ "$NODE_PATH" = "" ] && [ ! -d "node_modules" ]; then
     npm install
     # npm install --registry=https://registry.npm.taobao.org
 fi
