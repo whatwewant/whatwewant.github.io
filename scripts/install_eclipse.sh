b4783d180eab7d29151fc3c1d8273305b0fd0407
whatwewant <tobewhatwewant@gmail.com>
2015-08-02 14:36:57 +0800

    + install_eclipse.sh

--- scripts/install_eclipse.sh
+++ scripts/install_eclipse.sh
@@ -10,12 +10,12 @@ source $CURRENT_PATH/BaseFunctionSets.sh >> /dev/null 2>&1
 
 if [ "$(checkNetwork)" = "0" ]; then
     # download and install packages
-    PACKAGE_URL="http://download-cf.jetbrains.com/webstorm/WebStorm-10.0.2.tar.gz"
-    wgetToThenUntgz $PACKAGE_URL webstorm.tar.gz
+    PACKAGE_URL="http://mirrors.ustc.edu.cn/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz"
+    wgetToThenUntgz $PACKAGE_URL idea.tar.gz
 
     # soft link
     # sudo ln -s $PROGRAM_DIR/WebStorm-141.728/bin/webstorm.sh /usr/bin/webstorm
-    lnS WebStorm-141.728/bin/webstorm.sh webstorm
+    lnS eclipse/eclispe eclipse
 else
     echo "No Network."
     echo "Like:"
