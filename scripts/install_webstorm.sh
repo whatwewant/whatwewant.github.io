ccc57463607a7061dbb14fd720ce451f4c134297
cole <cole@mbuntu.com>
2015-08-02 14:25:24 +0800

    + install_webstorm.sh: modify

--- scripts/install_webstorm.sh
+++ scripts/install_webstorm.sh
@@ -8,9 +8,9 @@ PROGRAM_DIR=$HOME/.config/ProgramFiles
 source $CURRENT_PATH/BaseFunctionSets.sh
 
 # download and install packages
-PACKAGE_URL="http://download-cf.jetbrains.com/webstorm/WebStorm-10.0.2.tar.gz"
+PACKAGE_URL="http://download.jetbrains.com/webstorm/WebStorm-10.0.4.tar.gz"
 wgetToThenUntgz $PACKAGE_URL webstorm.tar.gz
 
 # soft link
 # sudo ln -s $PROGRAM_DIR/WebStorm-141.728/bin/webstorm.sh /usr/bin/webstorm
-lnS WebStorm-141.728/bin/webstorm.sh webstorm
+lnS WebStorm-141.1550/bin/webstorm.sh webstorm
