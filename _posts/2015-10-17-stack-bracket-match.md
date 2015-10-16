88848f770f0def94287db8489755dcca532014eb
whatwewant <tobewhatwewant@gmail.com>
2015-10-17 01:53:14 +0800

    + 2015-10-17-stack-bracket-match.md: 栈应用之括号匹配

---
+++ _posts/2015-10-17-stack-bracket-match.md
@@ -0,0 +1,66 @@
+---
+layout: post
+title: "栈应用 - 括号匹配"
+keywords: [""]
+description: ""
+category: ""
+tags: [""]
+---
+{% include JB/setup %}
+
+### 一、原理
+* 很简单，遇到左括号用栈存起来，遇到右括号就从栈取出一个比较即可.
+* 特殊情况:
+    * 在栈空的情况下遇到右括号肯定不匹配;
+    * 在括号扫描结束的情况下，栈不为空，肯定不匹配.
+
+### 二、步骤
+* Step 1: 从左到右扫描括号字符串
+* Step 2: 左括号入栈
+* Step 3: 判断右括号与栈顶元素是否匹配
+    * 如果存储左括号的栈空或不匹配，返回false
+    * 如果匹配出栈，扫描下一个字符
+* Step 4: 扫描结束后，如果栈不为空，则肯定不匹配
+
+### 三、实现
+
+```c
+int BRACKETS_VALUE[256];
+
+void init() {
+    BRACKETS_VALUE['('] = -1; BRACKETS_VALUE[')'] = 1;
+    BRACKETS_VALUE['['] = -2; BRACKETS_VALUE[']'] = 2;
+    BRACKETS_VALUE['{'] = -3; BRACKETS_VALUE['}'] = 3;
+}
+
+bool BracketsMatch(char *s) {
+    // 初始化BRACKETS_VALUE, 方便比较左右括号
+    // 左括号为负值，右括号为正值
+    // 每组括号，左右和为0
+    // 用处: 只要查看左右括号和是否为0即可判断是否匹配
+    init();
+    // 存储左括号的栈
+    stack<char> storeLeft;
+    // Step 1: 从左到右扫描括号字符串
+    for(int i=0; s[i]!='\0'; ++i) {
+        // Step 2: 左括号入栈
+        if (BRACKETS_VALUE[s[i]] < 0)
+            storeLeft.push(s[i]);
+        // Step 3: 右括号与栈顶元素是否匹配
+        //   如果存储左括号的栈空或不匹配，返回false
+        //   如果匹配出栈，扫描下一个字符
+        else {
+            if (storeLeft.empty() || \
+                    BRACKETS_VALUE[s[i]] + BRACKETS_VALUE[storeLeft.top()] != 0)
+                return false;
+            storeLeft.pop();
+        }
+    }
+    // Step 4: 栈是否为空 ? 匹配 : 不匹配;
+    if (! storeLeft.empty())
+        return false;
+    return true;
+}
+```
+
+### 四、[实例]({{site.url}}/work/algorithm/Stack-2-Bracket-Match.cpp)
---
+++ work/algorithm/Stack-2-Bracket-Match.cpp
@@ -0,0 +1,56 @@
+/****************************************************
+  > File Name    : Stack-2-Bracket-Match.cpp
+  > Author       : Cole Smith
+  > Mail         : tobewhatwewant@gmail.com
+  > Github       : whatwewant
+  > Created Time : 2015年10月17日 星期六 01时14分31秒
+  > Description  :
+    栈运用 之 括号匹配
+ ****************************************************/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int BRACKETS_VALUE[256];
+
+void init() {
+    BRACKETS_VALUE['('] = -1; BRACKETS_VALUE[')'] = 1;
+    BRACKETS_VALUE['['] = -2; BRACKETS_VALUE[']'] = 2;
+    BRACKETS_VALUE['{'] = -3; BRACKETS_VALUE['}'] = 3;
+}
+
+bool BracketsMatch(char *s) {
+    // 初始化BRACKETS_VALUE, 方便比较左右括号
+    // 左括号为负值，右括号为正值
+    // 每组括号，左右和为0
+    // 用处: 只要查看左右括号和是否为0即可判断是否匹配
+    init();
+    // 存储左括号的栈
+    stack<char> storeLeft;
+    // Step 1: 从左到右扫描括号字符串
+    for(int i=0; s[i]!='\0'; ++i) {
+        // Step 2: 左括号入栈
+        if (BRACKETS_VALUE[s[i]] < 0)
+            storeLeft.push(s[i]);
+        // Step 3: 右括号与栈顶元素是否匹配
+        //   如果存储左括号的栈空或不匹配，返回false
+        //   如果匹配出栈，扫描下一个字符
+        else {
+            if (storeLeft.empty() || \
+                    BRACKETS_VALUE[s[i]] + BRACKETS_VALUE[storeLeft.top()] != 0)
+                return false;
+            storeLeft.pop();
+        }
+    }
+    if (! storeLeft.empty())
+        return false;
+    return true;
+}
+
+int main (int argc, char* argv[]) {
+    // char s[] = "[([][])]";
+    char s[] = "[([{()[]{}}])]";
+    cout << BracketsMatch(s) << endl;
+    return 0;
+}
