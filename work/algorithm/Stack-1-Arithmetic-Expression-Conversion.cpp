0af77fe84d5a69f3dcba9e69374f7a3e8a1d9efe
cole <cole@mbuntu.com>
2015-10-17 01:53:14 +0800

    + 2015-10-17-stack-bracket-match.md: 栈应用之括号匹配

--- work/algorithm/Stack-1-Arithmetic-Expression-Conversion.cpp
+++ work/algorithm/Stack-1-Arithmetic-Expression-Conversion.cpp
@@ -1,30 +1,48 @@
----
-layout: post
-title: "栈应用 - 中缀表达式->后缀表达式"
-keywords: [""]
-description: ""
-category: algorithm
-tags: [algorithm, 数据结构, stack]
----
-{% include JB/setup %}
+/****************************************************
+  > File Name    : Stack-1-Arithmetic-Expression-Conversion.cpp
+  > Author       : Cole Smith
+  > Mail         : tobewhatwewant@gmail.com
+  > Github       : whatwewant
+  > Created Time : 2015年10月16日 星期五 23时40分32秒
+  > Description  : 
+    算术表达式转换之
+        中缀表达式 -> 后缀表达式
+ ****************************************************/
 
-### 一、原理
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
 
-### 二、步骤
-* Step 1: 从左向右开始扫描中缀表达式
-* Step 2: 如果是数字或字母，直接输出 或者 存取在其他地方
-* Step 3: 遇到运算符时, 3 IF
-    * 若为'('或栈空, 入栈;
-    * 若为')', 出栈直到'('为止;
-    * 若为其他, 比较运算符优先级, 
-        * 如果栈非空并且tmpC<=栈顶元素，栈顶元素出栈；
-        * 比较下一个栈顶元素, 直到栈空或左括号;
-        * 最后将tmpC入栈.
+/**
+ * @Desc    判断tmpC是不是字母或数字
+ * @param   char
+ * @return  是字母或数组, true; Or, false;
+ */
+bool isAlphaOrNumber(char tmpC) {
+    if (tmpC>='0' && tmpC<='9' ||\
+            tmpC>='a' && tmpC<='z' || \
+            tmpC>='A' && tmpC<='Z')
+        return true;
+    return false;
+}
 
-### 三、实现
+char LEVEL[100];
+/**
+ * @Desc 判断符号a的优先级是否高于b
+ * @param char 符号a
+ * @param char 符号b
+ * @return true/false
+ */
+bool highPriority(char a, char b) {
+    LEVEL['+'] = LEVEL['-'] = 1;
+    LEVEL['*'] = LEVEL['/'] = 2;
+    LEVEL['('] = 0;
+    // LEVEL[')'] = 0;
+    return LEVEL[a] > LEVEL[b];
+}
 
-```c
-nfixExpression2Postfix(string s) {
+void InfixExpression2Postfix(string s) {
     // 用于临时存放操作符的栈
     stack<char> operatorStack;
     
@@ -92,7 +110,14 @@ nfixExpression2Postfix(string s) {
     }
 
     cout << endl;
-}
-```
 
-### 四、[例子]({{site.url}/work/algorithm/Stack-1-Arithmetic-Expression-Conversion.cpp})
+}
+
+int main (int argc, char* argv[]) {
+    string s = "a+b-a*((c+d)/e-f)+g";
+    // string s = "a/b+(c*d-e*f)/g";
+    // string s;
+    // cin >> s;
+    InfixExpression2Postfix(s);
+    return 0;
+}
