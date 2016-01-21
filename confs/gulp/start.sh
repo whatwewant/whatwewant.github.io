7a6f98b96fdb579aed4d805a6d61b3181078485c
Cole Smith <uniquecolesmith@gmail.com>
2016-01-21 16:07:36 +0800

    + fix gulp/start.sh

--- confs/gulp/start.sh
+++ confs/gulp/start.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
+
+source $HOME/.zshrc
+
+nvm use v5.4.0
 
 if [ "$NODE_PATH" = "" ] && [ ! -d "node_modules" ]; then
     # npm install
-    npm install --registry=https://registry.npm.taobao.org
+    sudo npm install --registry=https://registry.npm.taobao.org
 fi
 
-gulp || (npm install && gulp)
+gulp || (sudo npm install && gulp)
