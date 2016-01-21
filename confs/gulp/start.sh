70354da521e4cf62acd763b3bd7387fa923359d7
Cole Smith <uniquecolesmith@gmail.com>
2016-01-21 15:37:20 +0800

    + update config_gulp_app.sh

--- confs/gulp/start.sh
+++ confs/gulp/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$NODE_PATH" = "" ] && [ ! -d "node_modules" ]; then
-    npm install
-    # npm install --registry=https://registry.npm.taobao.org
+    # npm install
+    npm install --registry=https://registry.npm.taobao.org
 fi
 
 gulp || (npm install && gulp)
