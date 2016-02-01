796b8e691394c34832bdb6706c5c343854ae9bed
whatwewant <tobewhatwewant@gmail.com>
2016-02-02 00:09:12 +0800

    + gulp: update base

--- confs/gulp/start.sh
+++ confs/gulp/start.sh
@@ -2,16 +2,22 @@
 
 source $HOME/.zshrc
 REGISTRY=https://registry.npm.taobao.org/
+# PREFIX=$HOME/.config/node_modules #
 
 nvm use v5.4.0
 
 if [ "$(npm config get registry)" != "$REGISTRY" ]; then
-    npm config set registry="$REGISTRY"
+    npm config set registry $REGISTRY
 fi
+
+# if [ "$(npm config get prefix)" != "$PREFIX" ]; then
+#  [[ ! -d $PREFIX ]] && mkdir -p $PREFIX
+#  npm config set prefix $PREFIX
+# fi
 
 if [ "$NODE_PATH" = "" ] && [ ! -d "node_modules" ]; then
     # npm install
-    sudo npm install # --registry=https://registry.npm.taobao.org
+    npm install # --registry=https://registry.npm.taobao.org
 fi
 
-gulp || (sudo npm install && gulp) # --registry=https://registry.npm.taobao.org && gulp)
+gulp || (npm install && gulp) # --registry=https://registry.npm.taobao.org && gulp)
