1e3e9855152be0d32893e8ff650e2804bcb54ecf
whatwewant <tobewhatwewant@gmail.com>
2016-06-15 02:01:06 +0800

    + update lets-encrypt_cmd.sh, add nginx server config

--- scripts/nginx/lets-encrypt/lets-encrypt_cmd.sh
+++ scripts/nginx/lets-encrypt/lets-encrypt_cmd.sh
@@ -53,6 +53,14 @@ DOMAIN_CRT=${DOMAIN}.crt
 APPLIED_SIGNED_CTR=${DOMAIN}.signed.crt
 DOMAIN_CSR=${DOMAIN}.csr
 DOMAIN_SSL_DIR=${DOMAIN_DIR}/.letsencrypt/${DOMAIN}
+#
+NGINX_SERVER_CONF_DIR=${DOMAIN_DIR}/config;
+NGINX_SERVER_CONF=${NGINX_SERVER_CONF_DIR}/${DOMAIN}.nginx.conf
+
+# 0 创建 NGINX SERVER CONF DIR
+[[ ! -d "$NGINX_SERVER_CONF_DIR" ]] && \
+    (mkdir -p $NGINX_SERVER_CONF_DIR || \
+        (echo "Error: #0 创建网站NGINX配置目录失败: $NGINX_SERVER_CONF_DIR" && exit -1))
 
 #1 创建临时配置目录, 并切换目录
 [[ ! -d "$CONF_DIR" ]] && \
@@ -151,36 +159,112 @@ echo "  生成配置: https://mozilla.github.io/server-side-tls/ssl-config-gener
 echo "  测试: https://www.ssllabs.com/ssltest/"
 echo "#################################"
 echo ""
-echo "  server {
-            listen 443 ssl;
-            server_name $DOMAIN;
-            charset utf-8;
-            client_max_body_size 100M;
-            server_tokens off;
+echo "
+server {
+    server_name www.$DOMAIN $DOMAIN;
+    server_tokens off;
 
-            ssl on;
-            ssl_certificate     $DOMAIN_SSL_DIR/$DOMAIN_CRT;
-            ssl_certificate_key $DOMAIN_SSL_DIR/$DOMAIN_KEY;
-            ssl_dhparam         $DOMAIN_SSL_DIR/dhparams.pem;
+    access_log /dev/null;
 
-            ssl_prefer_server_ciphers on;
-            ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+    if (\$request_method !~ ^(GET|HEAD|POST|PUT|DELETE)$) {
+        return 444;
+    }
 
-            ssl_protocols TLSv1 TLSV1.1 TLSV1.2;
-            
-            ssl_stapling on;
-            ssl_stapling_verify on;
-            ssl_trusted_certificate $DOMAIN_SSL_DIR/full_chained.pem;
+    location ^~ /.well-known/acme-challenge/ {
+        alias       $ACME_CHALLENGE_DIR;
+        try_files   $uri =404;
+    }
 
-            add_header Strict-Transport-Security \"max-age=31536000; includeSubdomains\";
+    location / {
+        rewrite     ^/(.*)$ https://$DOMAIN/\$1 permanent;
+    }
+}
 
-            ssl_session_timeout 1d;
-            ssl_session_cache shared:SSL:50m;
-            ssl_session_tickets off;
-"
+server {
+    listen 443 ssl;
+    server_name www.$DOMAIN $DOMAIN;
+    charset utf-8;
+    client_max_body_size 100M;
+    server_tokens off;
+
+    ssl on;
+    ssl_certificate     $DOMAIN_SSL_DIR/$DOMAIN_CRT;
+    ssl_certificate_key $DOMAIN_SSL_DIR/$DOMAIN_KEY;
+    ssl_dhparam         $DOMAIN_SSL_DIR/dhparams.pem;
+
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+
+    ssl_protocols TLSv1 TLSV1.1 TLSV1.2;
+    
+    ssl_stapling on;
+    ssl_stapling_verify on;
+    ssl_trusted_certificate $DOMAIN_SSL_DIR/full_chained.pem;
+
+    add_header Strict-Transport-Security \"max-age=31536000; includeSubdomains\";
+
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:SSL:50m;
+    ssl_session_tickets off;
+
+    resolver            114.114.114.114 valid=300s;
+    resolver_timeout    10s;
+    
+    access_log          $DOMAIN_DIR/.$DOMAIN.log;
+
+    if (\$request_method !~ ^(GET|HEAD|POST|OPTIONS|PUT|DELETE)$) {
+        return          444;
+    }
+
+    if (\$host != '$DOMAIN') {
+        rewrite ^/(.*)$ https://$DOMAIN/\$1 permanent;
+    }
+
+    location ~* (robots\\.txt|favicon\\.ico|crossdomain\\.xml|google4c90d18e696bdcf8\\.html|BingSiteAuth\\.xml)\$  {
+        root        $DOMAIN_DIR/static;
+        expires     1d;
+    }
+
+    location ^~ /static/ {
+        root        $DOMAIN_DIR/static;
+        add_header  Access-Control-Allow-Origin *;
+        expires     max;
+    }
+
+    location / {
+        proxy_http_version 1.1;
+
+        add_header  Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\";
+        add_header  -Frame-Options deny;
+        add_header  X-Content-Type-Options nosniff;
+        add_header  Content-Security-Policy \"default-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' blob: https:; img-src data: https: http://ip.qgy18.com; style-src 'unsafe-inline' https:; child-src https:; connect-src 'self' https://translate.googleapis.com; frame-src https://disqus.com https://www.slideshare.net\";
+        add_header  Public-Key-Pins 'pin-sha256=\"YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=\"; pin-sha256=\"aef6IF2UF6jNEwA2pNmP7kpgT6NFSdt7Tqf5HzaIGWI=\"; max-age=2592000; includeSubDomains';
+        add_header  Cache-Control no-cache;
+
+        proxy_ignore_headers    Set-Cookie;
+
+        proxy_hide_header       Vary;
+        proxy_hide_header       X-Powered-By;
+
+        proxy_set_header        X-Via       Qingdao.Aliyun;
+        proxy_set_header        Connection  \"\";
+        proxy_set_header        Host        $DOMAIN;
+        proxy_set_header        X-Real_IP   $remote_addr;
+        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        proxy_pass              http://127.0.0.1:9125;
+    }
+}
+" | tee  $NGINX_SERVER_CONF
+echo ""
+echo "######################################################"
+echo "  1. 请修改 $NGINX_SERVER_CONF 以适应你的网站."
+echo "  2. 在/etc/nginx/nginx.conf的http底部加入服务器配置: "
+echo "      include $NGINX_SERVER_CONF_DIR/*.conf"
+echo "######################################################"
 echo ""
 
-# Cron 定时更新证书
+# Cron 定时更新证书(每月)
 # crontab -e
-# 0 0 1 * * /etc/nginx/certs/letsencrypt.sh /etc/nginx/certs/letsencrypt.conf >> /var/log/lets-encrypt.log 2>&1
+# 0 0 1 * * /etc/nginx/certs/letsencrypt_cmd.sh $DOMAIN $DOMAIN_DIR "DNS:$DOMAIN,DNS:www.$DOMAIN" >> /var/log/lets-encrypt.log 2>&1
 
